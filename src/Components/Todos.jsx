aceb037025c1a022ea4ac0af67d4a29a68c842e9
omkundu <omkundu156@gmail.com>
2023-04-07 12:51:21 +0530

    seventh-commit

--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import axios from "axios"
+import { useDispatch, useSelector } from 'react-redux'
+import { getTaskFailure, getTaskRequest, getTaskSuccess } from '../Redux/action'
 const Todos = () => {
+
+    const dispatch=useDispatch()
+    const todos=useSelector((state)=>state.todos)
+
+    const getTask=()=>{
+
+        dispatch(getTaskRequest())
+            axios.get('http://localhost:8080/todos').then((r)=>{
+               dispatch(getTaskSuccess(r.data))
+              console.log(r.data)
+            }).catch(e=>{
+                // console.log(err)
+                dispatch(getTaskFailure(e))
+            });
+    };
+
+    console.log(todos)
+
+     useEffect(()=>{
+        getTask()
+    },[]);
+
+
   return (
     <div>
-      
+      <h3>Todos</h3>
+      {
+        todos.length>0&&todos.map(item=>{
+            return <div key={item.id}> 
+                {item.title} - {item.status?"True":"False"}
+            </div>
+        })
+      }
     </div>
   )
 }
